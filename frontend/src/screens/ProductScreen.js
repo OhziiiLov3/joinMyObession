1bd118b17bbb477aaad2cea5cabb4298b01eb810
ohziiilov3 <klbaskerville0520@gmail.com>
2022-06-21 09:53:31 -0700

    added useParams for ProductScreen Component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Link, useParams} from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import trainings from '../trainings'
 
-function ProductScreen() {
+function ProductScreen({ match }) {
+    const training_id = useParams();
+    const training = trainings.find((t) => t._id === training_id.id)
+
+
+
     return (
         <div>
-            Product 
+           {training.name}
         </div>
     )
 }
