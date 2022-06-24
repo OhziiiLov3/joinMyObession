9bf72b834f73e88f8141af4755b2d2034d76d764
ohziiilov3 <klbaskerville0520@gmail.com>
2022-06-23 18:20:32 -0700

    created submain branch to backup previous code

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -2,66 +2,24 @@ import React from 'react'
 
 function Rating({value, text, color}) {
     return (
-        <div className="rating">
-            <span>
-                <i style={{color}} className={
-                    value >= 1 
-                        ? 'fas fa-star'
-                        : value >= 0.5 
-                            ? 'fas fa-star-half-alt'
-                            :'far- fa-star'
-                }>
-
-                </i>
-            </span>
-            <span>
-                <i style={{ color }} className={
-                    value >= 2
-                    ? 'fas fa-star'
-                    :value >= 1.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-                }>
-
-                </i>
-            </span>
-            <span>
-                <i style={{ color }} className={
-                    value >= 3
-                    ? 'fas fa-star'
-                    :value >= 2.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-                }>
-
-                </i>
-            </span>
-            <span>
-                <i style={{ color }} className={
-                    value >= 4
-                    ? 'fas fa-star'
-                    :value >= 3.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-                }>
-
-                </i>
-            </span>
-            <span>
-                <i style={{ color }} className={
-                    value >= 5
-                    ? 'fas fa-star'
-                    :value >= 4.5
-                        ? 'fas fa-star-half-alt'
-                        : 'far fa-star'
-                }>
-
-                </i>
-            </span>
-            <span>{text && text}</span>
-            
-        </div>
-    )
+      <div className="rating">
+        <span>
+          <i style={{ color }} className="fa-solid fa-dumbbell" size="3x"></i>
+        </span>
+        <span>
+          <i style={{ color }} className="fa-solid fa-dumbbell" size="3x"></i>
+        </span>
+        <span>
+          <i style={{ color }} className="fa-solid fa-dumbbell" size="3x"></i>
+        </span>
+        <span>
+          <i style={{ color }} className="fa-solid fa-dumbbell" size="3x"></i>
+        </span>
+        <span>
+        </span>
+        <span>{text && text}</span>
+      </div>
+    );
 }
 
 export default Rating
--- frontend/src/components/Training.js
+++ frontend/src/components/Training.js
@@ -6,30 +6,30 @@ import {Link} from 'react-router-dom'
 
 function Training({ training }) {
     return (
-            <Card  className="mx-auto p-4 ">
-            <Link to={`/training/${training._id}`}> 
-                <Card.Img src={training.image} />           
-            </Link>
-            <Card.Body>
-                <Link to={`/training/${training._id}`}>
-                    <Card.Title as="div">
-                    <strong>{training.name}</strong>
-                    </Card.Title>
-                </Link>
-            <Card.Text as="div">
-                   <div className="my-3">
-                       <Rating value={training.rating} text={`${training.numReviews} Reviews` } color={'#f8e825'} />
-                   </div>
-            </Card.Text> 
-            <Card.Text as="h3">
-            ${training.price}
-            </Card.Text>
-
-            </Card.Body>
-
-            </Card>  
-            
-    )
+      <Card className="mx-auto p-4 ">
+        <Link to={`/training/${training._id}`}>
+          <Card.Img src={training.image} />
+        </Link>
+        <Card.Body>
+          <Card.Text className="text-center py-2" as="p">{training.bookingInfo}</Card.Text>
+          <Link
+            className="btn my-2 d-block btn-success"
+            to={`/training/${training._id}`}
+          >
+            <Card.Title as="div">{training.name}</Card.Title>
+          </Link>
+          <Card.Text as="div">
+            <div className="my-3">
+              {/* <Rating
+                value={training.rating}
+                text={`${training.numReviews} Reviews`}
+                color={"#f8e825"}
+              /> */}
+            </div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    );
 }
 
 export default Training
