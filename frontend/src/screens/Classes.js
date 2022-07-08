ff2854d4c8b30024c767332077b5b5fefc524a9b
ohziiilov3 <klbaskerville0520@gmail.com>
2022-07-07 21:31:08 -0700

    working on classes component

--- frontend/src/screens/Classes.js
+++ frontend/src/screens/Classes.js
@@ -6,39 +6,32 @@ function Classes() {
       <div className="d-flex justify-content-center my-5">
         <h1>My Services</h1>
       </div>
-        <h2 className="mx-auto py-3">Interactive Online Fitness Programs</h2>
+      <h2 className="mx-auto py-1">Interactive Online Fitness Programs</h2>
       <div className="container myClasses-wrapper">
-        <div className="timeline-block timeline-block-right">
+        {/*  */}
+        <div className="timeline-block timeline-block-left">
           <div className="marker">
             <div className="timeline-content">
-              <h3>Hiit</h3>
+              <h3 className="py-2">Hiit</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                esse fugit minima corporis beatae aliquid?
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+                quasi aliquam illum enim maiores praesentium ducimus asperiores,
+                blanditiis nostrum, nemo delectus tempora accusantium harum,
+                earum iste dicta vero ut a?
               </p>
             </div>
           </div>
         </div>
         {/*  */}
-        <div className="timeline-block timeline-block-left">
+        <div className="timeline-block timeline-block-right">
           <div className="marker">
             <div className="timeline-content">
               <h3>Plyometric Training</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                esse fugit minima corporis beatae aliquid?
-              </p>
-            </div>
-          </div>
-        </div>
-        {/*  */}
-        <div className="timeline-block timeline-block-right">
-          <div className="marker">
-            <div className="timeline-content">
-              <h3>Fat Burning and Muscle Scuplting</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                esse fugit minima corporis beatae aliquid?
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+                quasi aliquam illum enim maiores praesentium ducimus asperiores,
+                blanditiis nostrum, nemo delectus tempora accusantium harum,
+                earum iste dicta vero ut a?
               </p>
             </div>
           </div>
@@ -47,10 +40,26 @@ function Classes() {
         <div className="timeline-block timeline-block-left">
           <div className="marker">
             <div className="timeline-content">
+              <h3>Fat Burning and Muscle Scuplting</h3>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+                quasi aliquam illum enim maiores praesentium ducimus asperiores,
+                blanditiis nostrum, nemo delectus tempora accusantium harum,
+                earum iste dicta vero ut a?
+              </p>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker">
+            <div className="timeline-content">
               <h3>KickBoxing</h3>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                esse fugit minima corporis beatae aliquid?
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+                quasi aliquam illum enim maiores praesentium ducimus asperiores,
+                blanditiis nostrum, nemo delectus tempora accusantium harum,
+                earum iste dicta vero ut a?
               </p>
             </div>
           </div>
