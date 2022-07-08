dc430bccbeaf5311ed8a053ab9f2c5df0a391ebf
ohziiilov3 <klbaskerville0520@gmail.com>
2022-07-07 21:40:35 -0700

    fixed marker bug, moved to div to sameline(I put the timeline-content inside the marker

--- frontend/src/screens/Classes.js
+++ frontend/src/screens/Classes.js
@@ -10,58 +10,54 @@ function Classes() {
       <div className="container myClasses-wrapper">
         {/*  */}
         <div className="timeline-block timeline-block-left">
-          <div className="marker">
-            <div className="timeline-content">
-              <h3 className="py-2">Hiit</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
-                quasi aliquam illum enim maiores praesentium ducimus asperiores,
-                blanditiis nostrum, nemo delectus tempora accusantium harum,
-                earum iste dicta vero ut a?
-              </p>
-            </div>
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3 className="py-2">Hiit</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+              quasi aliquam illum enim maiores praesentium ducimus asperiores,
+              blanditiis nostrum, nemo delectus tempora accusantium harum, earum
+              iste dicta vero ut a?
+            </p>
           </div>
         </div>
         {/*  */}
         <div className="timeline-block timeline-block-right">
-          <div className="marker">
-            <div className="timeline-content">
-              <h3>Plyometric Training</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
-                quasi aliquam illum enim maiores praesentium ducimus asperiores,
-                blanditiis nostrum, nemo delectus tempora accusantium harum,
-                earum iste dicta vero ut a?
-              </p>
-            </div>
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>Plyometric Training</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+              quasi aliquam illum enim maiores praesentium ducimus asperiores,
+              blanditiis nostrum, nemo delectus tempora accusantium harum, earum
+              iste dicta vero ut a?
+            </p>
           </div>
         </div>
         {/*  */}
         <div className="timeline-block timeline-block-left">
-          <div className="marker">
-            <div className="timeline-content">
-              <h3>Fat Burning and Muscle Scuplting</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
-                quasi aliquam illum enim maiores praesentium ducimus asperiores,
-                blanditiis nostrum, nemo delectus tempora accusantium harum,
-                earum iste dicta vero ut a?
-              </p>
-            </div>
+          <div className="marker"> </div>
+          <div className="timeline-content">
+            <h3>Fat Burning and Muscle Scuplting</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+              quasi aliquam illum enim maiores praesentium ducimus asperiores,
+              blanditiis nostrum, nemo delectus tempora accusantium harum, earum
+              iste dicta vero ut a?
+            </p>
           </div>
         </div>
         {/*  */}
         <div className="timeline-block timeline-block-right">
-          <div className="marker">
-            <div className="timeline-content">
-              <h3>KickBoxing</h3>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
-                quasi aliquam illum enim maiores praesentium ducimus asperiores,
-                blanditiis nostrum, nemo delectus tempora accusantium harum,
-                earum iste dicta vero ut a?
-              </p>
-            </div>
+          <div className="marker"> </div>
+          <div className="timeline-content">
+            <h3>KickBoxing</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas
+              quasi aliquam illum enim maiores praesentium ducimus asperiores,
+              blanditiis nostrum, nemo delectus tempora accusantium harum, earum
+              iste dicta vero ut a?
+            </p>
           </div>
         </div>
       </div>
