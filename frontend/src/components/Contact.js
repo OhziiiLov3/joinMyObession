8e026c5f370d80b9745879e827dd8103234af2e0
ohziiilov3 <klbaskerville0520@gmail.com>
2022-07-12 12:52:55 -0700

    adding inputs and textarea to contact form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -11,6 +11,57 @@ function Contact() {
           nemo?
         </p>
       </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 col-xs-12">
+            {/* NAME INPUT */}
+            <input
+              id="name"
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              name="name"
+            />
+            {/* PHONE INPUT */}
+            <input
+              id="phone"
+              text="text"
+              className="form-control"
+              placeholder="Phone Number"
+              phone="phone"
+            />
+            {/* EMAIL INPUT */}
+            <input
+              id="email"
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              email="email"
+            />
+            {/* Message/Subject*/}
+            <input
+              id="message"
+              text="text"
+              className="form-control"
+              placeholder="Message"
+              Message="Message"
+            />
+          </div>
+          <div className="col-md-6 col-xs-12">
+            {/* Description */}
+            <textarea
+              id="description"
+              text="text"
+              className="form-control"
+              placeholder="Description"
+              Description="Description"
+            ></textarea>
+            <button className="btn btn-success contact-btn" type="submit">
+              Connect
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
