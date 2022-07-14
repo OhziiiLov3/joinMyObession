79882893383563b437c9d466bc6127b5e5396ea0
ohziiilov3 <klbaskerville0520@gmail.com>
2022-07-14 12:52:37 -0700

    email js installed and test working onclick

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,9 +1,27 @@
-import React from 'react';
-
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+  const serviceID = "service_ID";
+  const templateID = "JMOtemplate_ID";
+  const userID = "user_yd0HoddbkToxCxmJDWBi6";
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(serviceID, templateID, form.current, userID).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
-    <div id="contact" className="contact mx-auto py-4">
+    <div id="contact" className="contact mx-auto py-4 ">
       <div className="text-center">
         <h1>Contact Me</h1>
         <p>
@@ -12,62 +30,69 @@ function Contact() {
         </p>
       </div>
       <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-xs-12">
-            {/* NAME INPUT */}
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              placeholder="Name"
-              name="name"
-            />
-            <div className="line"></div>
-            {/* PHONE INPUT */}
-            <input
-              id="phone"
-              text="text"
-              className="form-control"
-              placeholder="Phone Number"
-              phone="phone"
-            />
-            <div className="line"></div>
-            {/* EMAIL INPUT */}
-            <input
-              id="email"
-              type="email"
-              className="form-control"
-              placeholder="Email"
-              email="email"
-            />
-            <div className="line"></div>
-            {/* Message/Subject*/}
-            <input
-              id="message"
-              text="text"
-              className="form-control"
-              placeholder="Subject"
-              message="message"
-            />
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="row">
+            <div className="col-md-6 col-xs-12">
+              {/* NAME INPUT */}
+              <div className="text-center"></div>
+              <input
+                id="name"
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                name="name"
+              />
+              <div className="line"></div>
+              {/* PHONE INPUT */}
+              <div className="text-center"></div>
+              <input
+                id="phone"
+                text="text"
+                className="form-control"
+                placeholder="Phone Number"
+                name="phone"
+              />
+              <div className="line"></div>
+              {/* EMAIL INPUT */}
+              <div className="text-center"></div>
+              <input
+                id="email"
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                name="email"
+              />
+              <div className="line"></div>
+              {/* Message/Subject*/}
+              <div className="text-center"></div>
+              <input
+                id="message"
+                text="text"
+                className="form-control"
+                placeholder="Subject"
+                name="message"
+              />
+            </div>
+            <div className="col-md-6 col-xs-12">
+              {/* Description */}
+              <div id="contact-link" className="text-center"></div>
+              <textarea
+                id="description"
+                text="text"
+                className="form-control"
+                placeholder="Description"
+                name="description"
+              ></textarea>
+              {/* <div className="line"></div> */}
+              <button className="btn btn-success contact-btn" type="submit">
+                Connect
+              </button>
+            </div>
           </div>
-          <div className="col-md-6 col-xs-12">
-            {/* Description */}
-            <textarea
-              id="description"
-              text="text"
-              className="form-control"
-              placeholder="Description"
-              description="description"
-            ></textarea>
-            {/* <div className="line"></div> */}
-            <button className="btn btn-success contact-btn" type="submit">
-              Connect
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
 }
 
-export default Contact
+export default Contact;
